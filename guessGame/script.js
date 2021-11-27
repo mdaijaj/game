a1d382cd2ca5d666b43d0cc39eae06e378c28e2d
mdaijaj <aijaj18@navgurukul.org>
2021-11-27 23:20:18 +0530

    ternary operator

--- guessGame/script.js
+++ guessGame/script.js
@@ -1,14 +1,12 @@
-
-let score=10;
-let highScore=0;
-var audio= new Audio('/start.mp3')
-audio.play()
 // let winaudio= new Audio('./assets/win.mp3')
 // let loseAudio= new Audio('./assets/lose.wav')
 // let tap= new Audio('./assets/tap.mp3')
-
+var audio= new Audio('/start.mp3')
+let score=5;
+let highScore=0;
 let secretNo= Math.floor((Math.random()*50)+1)
 let guess=document.querySelector('.guess').textContent= '?';
+
 
 audio.play();
 document.querySelector('.check').addEventListener('click', ()=>{
@@ -17,38 +15,27 @@ document.querySelector('.check').addEventListener('click', ()=>{
         document.querySelector('.message').textContent= "No Number!"
     }
     else if(guessNum==secretNo){
-        const currentScore=document.querySelector('.score').textContent=score;
-        console.log("currentScore", currentScore)
+        document.querySelector('body').style.backgroundColor= "green"
         document.querySelector('.message').textContent= `Number match successfully!`;
+        document.querySelector('.score').textContent=score;
         document.querySelector('.highscore').textContent= score;
         document.querySelector('.number').textContent= secretNo;
-        document.querySelector('body').style.backgroundColor= "green"
     }
-    else if(score<1){
-        document.querySelector('.message').textContent= "Game over!"
-        document.querySelector('body').style.backgroundColor= "red"
-
-    }
-    else if(guessNum>secretNo){
+    else if(score>1){
+        document.querySelector('.message').textContent= 
+        guessNum>secretNo? "it should be less then your input!" : "it should be greater then your input!";
         score--
         const currentScore=document.querySelector('.score').textContent=score;
-        console.log("currentScore", currentScore)
-        document.querySelector('.message').textContent= "it should be less then your input!"
-    }
-    else if(guessNum<secretNo){
-        score--
-        const currentScore=document.querySelector('.score').textContent=score;
-        console.log("currentScore", currentScore)
-        document.querySelector('.message').textContent= "it should be greater then your input!"
     }
     else{
-        document.querySelector('.message').textContent= "please carefull lock your account!"
+        document.querySelector('.message').textContent= "Game over!"
+        document.querySelector('body').style.backgroundColor= "red"
     } 
 })
 
 //again work again
 document.querySelector('.again').addEventListener('click', ()=>{
-    score=10;
+    score=5;
     let secretNo= Math.floor((Math.random()*50)+1)
     document.querySelector('body').style.backgroundColor= "gray"
     document.querySelector('.number').textContent='?';
